14d534650aecd98ef873a4c364bffe8c87635b94
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-11 13:51:01 +0600

    day0 checked

---
+++ src/main/java/day0/Solution.java
@@ -0,0 +1,13 @@
+package day0;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+        scan.close();
+    }
+}
