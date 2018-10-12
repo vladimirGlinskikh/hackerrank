fbfdb6c4613f53a5a88774b2e90446911ee936cb
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-10-12 12:16:25 +0600

    day1 try one.

---
+++ src/main/java/day1/Solution.java
@@ -0,0 +1,23 @@
+package day1;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank";
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        Scanner scanner0 = new Scanner(System.in).useLocale(Locale.UK);
+        double b = scanner0.nextDouble();
+        Scanner scanner = new Scanner(System.in);
+        String st = scanner.nextLine();
+
+        System.out.println(i + a);
+        System.out.println(d + b);
+        System.out.println(s + " " + st);
+        scan.close();
+    }
+}
